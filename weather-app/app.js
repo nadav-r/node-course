53978043d52bf3cbb50392636a69ac24632ecca1
nadav <nadavra@gmail.com>
2019-08-27 06:45:39 -0700

    add privacy

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,6 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-const secretKeyDarkSky = 'd5ce81066d903c4020d272c3856845f1';
-const tokenMapBox = 'pk.eyJ1IjoibmFkMjIiLCJhIjoiY2p4b3BpM2U2MDg1cTNicGY4dmUwMmozdCJ9.9f9O9xMvX9rcbth_4DR5og';
 
 let address = process.argv[2];
 if(!address){
