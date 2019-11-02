34349451b6592766e0024080d0d6c2aeb7e0f56a
nadav-r <nadavra@gmail.com>
2019-11-02 13:32:04 +0200

    mongodb stuff

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,19 +1,21 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
-
+const { MongoClient, ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database')
     }
 
     const db = client.db(databaseName)
-    
-    db.collection('usrs').insertOne({
-        name: 'Nadav',
-        age: 30
-    })
+    // db.collection('users').findOne({ _id: new ObjectID("5dbd4169cca775110c533d21") }, (error, user) => {
+    //     if (error) {
+    //         return console.log('unable to fetch')
+    //     }
+    //     console.log(user)
+    // })
 
+    db.collection('users').find({ age: 27 }).toArray((error, users) => {
+        console.log(users)
+    })
 })
